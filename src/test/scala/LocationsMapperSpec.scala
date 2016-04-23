0f4993d7bace0841bca9cf7d29ebf09ad94e1636
Alberto Rodriguez <arodriguez@stratio.com>
2016-04-23 22:44:47 +0200

    First failing test for the findCoordinates method

--- src/test/scala/LocationsMapperSpec.scala
+++ src/test/scala/LocationsMapperSpec.scala
@@ -3,16 +3,34 @@ import org.scalatest.Matchers._
 
 class LocationsMapperSpec extends FunSpec {
 
-  describe("The location mapper") {
+  describe("The locations mapper") {
 
-    it("should say hello") {
-      val greeting = LocationsMapper.sayHello()
-      greeting shouldBe "hello"
+    it("should find out the coordinates for an existing id") {
+      val id1 = "id1"
+      val id2 = "id1"
+      val xCoord1 = "1234,45"
+      val xCoord2 = "6874,89"
+      val yCoord1 = "6789,45"
+      val yCoord2 = "6789,45"
+      val coordinatesMap: Map[String, (String, String)] = Map((id1, (xCoord1, yCoord1)), (id2, (xCoord2, yCoord2)))
+      val element1 = "78"
+      val element2 = "30"
+      val pointsInfo = List((id1, element1), (id2, element2))
+      val expectedCoordinates = List(
+        (id1, element1, xCoord1, yCoord1),
+        (id2, element2, xCoord2, yCoord2))
+      val coordinates = LocationsMapper.findCoordinates(pointsInfo, coordinatesMap)
+
+      coordinates should contain theSameElementsAs(expectedCoordinates)
     }
   }
 }
 
 object LocationsMapper {
 
-  def sayHello(): String = "hello"
+  def findCoordinates(
+     pointsInfo: List[(String, String)],
+     coordinatesMap: Map[String, (String, String)]): List[(String, String, String, String)] = {
+    List(("", "", "", ""))
+  }
 }
