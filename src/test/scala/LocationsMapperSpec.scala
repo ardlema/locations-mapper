98678806d31032249d934d12eb1231744d6d7272
Alberto Rodriguez <arodriguez@stratio.com>
2016-04-23 22:58:15 +0200

    Add test for non existing ids

--- src/test/scala/LocationsMapperSpec.scala
+++ src/test/scala/LocationsMapperSpec.scala
@@ -5,20 +5,38 @@ class LocationsMapperSpec extends FunSpec {
 
   describe("The locations mapper") {
 
-    it("should find out the coordinates for an existing id") {
+    it("should find out the coordinates for existing ids") {
       val id1 = "id1"
       val id2 = "id2"
       val xCoord1 = "1234,45"
       val xCoord2 = "6874,89"
       val yCoord1 = "6789,45"
       val yCoord2 = "4542,01"
-      val coordinatesMap: Map[String, (String, String)] = Map((id1, (xCoord1, yCoord1)), (id2, (xCoord2, yCoord2)))
+      val coordinatesMap = Map((id1, (xCoord1, yCoord1)), (id2, (xCoord2, yCoord2)))
       val element1 = "78"
       val element2 = "30"
       val pointsInfo = List((id1, element1), (id2, element2))
       val expectedCoordinates = List(
         (id1, element1, xCoord1, yCoord1),
         (id2, element2, xCoord2, yCoord2))
+      val coordinates = LocationsMapper.findCoordinates(pointsInfo, coordinatesMap)
+
+      coordinates should contain theSameElementsAs(expectedCoordinates)
+    }
+
+    it("should not fail when there is no coordinates for an id") {
+      val id1 = "id1"
+      val id2 = "id2"
+      val nonExistingId = "nonExistingId"
+      val xCoord1 = "1234,45"
+      val xCoord2 = "6874,89"
+      val yCoord1 = "6789,45"
+      val yCoord2 = "4542,01"
+      val coordinatesMap = Map((id1, (xCoord1, yCoord1)), (id2, (xCoord2, yCoord2)))
+      val element1 = "78"
+      val element2 = "30"
+      val pointsInfo = List((id1, element1), (nonExistingId, element2))
+      val expectedCoordinates = List((id1, element1, xCoord1, yCoord1))
       val coordinates = LocationsMapper.findCoordinates(pointsInfo, coordinatesMap)
 
       coordinates should contain theSameElementsAs(expectedCoordinates)
@@ -32,7 +50,8 @@ object LocationsMapper {
      pointsInfo: List[(String, String)],
      coordinatesMap: Map[String, (String, String)]): List[(String, String, String, String)] = {
     for (pointInfo <- pointsInfo;
-         coordinates = coordinatesMap.get(pointInfo._1)
+         coordinates = coordinatesMap.get(pointInfo._1);
+         if coordinates.isDefined
     ) yield (
       pointInfo._1,
       pointInfo._2,
